d479acb6a05c639aec082c9d51acf39f6743d667
Vlad Ciobanu <ciobanu.vlad@yahoo.com>
2020-02-24 23:45:17 +0100

    added filter by types on pipe

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -13,8 +13,27 @@ export class HomePageComponent implements OnInit {
   ) { }
 
   public gnoms;
-  search: string;
+  search: any;
+  searchType: string;
   isData = false;
+  name: string;
+
+  selectTypes = [
+    {key: 'id', value: 'Id'},
+    {key: 'name', value: 'Name'},
+    {key: 'age', value: 'Age'},
+    {key: 'olderThan', value: 'Older Than'},
+    {key: 'youngerThan', value: 'Younger Than'},
+    {key: 'weight', value: 'Weight'},
+    {key: 'weightMoreThan', value: 'Weight more than'},
+    {key: 'weightLessThan', value: 'Weight less than'},
+    {key: 'height', value: 'Height'},
+    {key: 'heigherThan', value: 'Heigher Than'},
+    {key: 'heightUnder', value: 'Height under'},
+    {key: 'hair_color', value: 'Hair Color'},
+    {key: 'professions', value: 'Professions'},
+    {key: 'friends', value: 'Friends'}
+  ];
 
   ngOnInit() {
     this.getGnoms();
--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], searchText: string): any[] {
+  transform(items: any[], searchText: string, searchType: string): any[] {
     if (!items) {
       return [];
     }
@@ -14,7 +14,67 @@ export class FilterPipe implements PipeTransform {
     }
     searchText = searchText.toLowerCase();
     return items.filter( it => {
-      return it.name.toLowerCase().includes(searchText);
+      switch (searchType) {
+        case 'name': {
+          return it.name.toLowerCase().includes(searchText);
+          break;
+        }
+        case 'id': {
+          return it.id === Number(searchText);
+          break;
+        }
+        case 'age': {
+          return it.age === Number(searchText);
+          break;
+        }
+        case 'olderThan': {
+          return it.age >= Number(searchText);
+          break;
+        }
+        case 'youngerThan': {
+          return it.age <= Number(searchText);
+          break;
+        }
+        case 'weight': {
+          return it.weight === Number(searchText);
+          break;
+        }
+        case 'weightMoreThan': {
+          return it.weight >= Number(searchText);
+          break;
+        }
+        case 'weightLessThan': {
+          return it.weight <= Number(searchText);
+          break;
+        }
+        case 'height': {
+          return it.height === Number(searchText);
+          break;
+        }
+        case 'heigherThan': {
+          return it.height >= Number(searchText);
+          break;
+        }
+        case 'heightUnder': {
+          return it.height <= Number(searchText);
+          break;
+        }
+        case 'hair_color': {
+          return it.hair_color.toLowerCase().includes(searchText);
+          break;
+        }
+        case 'professions': {
+          return it.professions.find(professions => professions.toLowerCase().includes(searchText));
+          break;
+        }
+        case 'friends': {
+          return it.friends.find(friend => friend.toLowerCase().includes(searchText));
+          break;
+        }
+        default:
+          return items;
+          break;
+      }
     });
    }
 }
